0e84dc8fc5427f8385b26d5765a991c845031c0e
pratp-123 <prateekchauhan923@gmail.com>
2024-05-02 09:10:48 +0530

    added area feature

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from calc_func import do_addition,do_sub
-
+from area import calculate_area_rectangle
 def main():
     print("Welcome to the calculator app")
     print("""\nSelect the function from given option
           1. Add 
           2. Substract
+          3. area
           """)
 
     user_input = input("Select the function")
@@ -15,7 +16,8 @@ def main():
         result = do_addition(a,b)
     elif user_input == "2":
         result = do_sub(a,b)
-    
+    elif user_input == "2":
+        result = calculate_area_rectangle(a,b)
     print("result",result)
 
 
